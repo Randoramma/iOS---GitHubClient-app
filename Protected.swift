2fc28502ca5d1bc9b6006186a8c932242567fef6
Randy McLain <Randoramma@me.com>
2015-04-16 07:48:40 -0700

    setting up Oauth

--- Protected.swift
+++ Protected.swift
@@ -8,4 +8,6 @@
 
 import Foundation
 
+let myClientID = "cd18b0b416f51c38c3e4"
 
+let myClientSecretKey = "3a3fb179d8e6548d0a347dc3b32783f7b5ea902a"
--- ReposSearchViewController.swift
+++ ReposSearchViewController.swift
@@ -8,37 +8,55 @@
 
 import UIKit
 
-class ReposSearchViewController: UIViewController {
+class ReposSearchViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate {
 
-    let myGitHubService = GitHubService()
+  @IBOutlet weak var mySearchBar: UISearchBar!
   
+  @IBOutlet weak var mySearchTableView: UITableView!
+  
+  let myGitHubService = GitHubService()
+ 
+  var myResults = [Repository]()
     override func viewDidLoad() {
         super.viewDidLoad()
       
-      self.myGitHubService.fetchReposForSearch("Swift", completionHandler: { (repos, error) -> (Void) in
-        // if we get 
-        if repos != nil {
-         // println(repos!.count)
-        }
-      }) // fetchReposForSearch
-
-        // Do any additional setup after loading the view.
+      self.mySearchTableView.dataSource = self
+      self.mySearchBar.delegate = self
+      
     } // viewDidLoad
 
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.myResults.count
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCellWithIdentifier("SearchTableCell", forIndexPath: indexPath) as! UITableViewCell
+    
+    let theRepo = self.myResults[indexPath.row]
+    cell.textLabel?.text = theRepo.name
+    return cell
+  }
+  
+  func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+    
+    mySearchBar.resignFirstResponder()
+    
+    self.myGitHubService.fetchReposForSearch(mySearchBar.text, completionHandler: { (repos, error) -> (Void) in
+      // if we get
+      if repos != nil {
+        //println(repos!.count)
+        self.myResults = repos!
+        self.mySearchTableView.reloadData()
+      }
+    }) // fetchReposForSearch
+  }
+  
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     } // didReceiveMemoryWarning
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
